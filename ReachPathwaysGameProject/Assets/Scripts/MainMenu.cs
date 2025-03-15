63ccb515bfd0cf9a23ada10c4e27cb08913b48f7
Evy Avila <384036@colum.edu>
2025-03-14 23:10:17 -0500

    Set up scenes, folders, packages and placeholder canvas for lo-fi main menu

---
+++ ReachPathwaysGameProject/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    /* TODO: Create Lo-fi main menu
+     * Simple layout with placeholder buttons to navigate menu.
+     * Take user to correct scene or display information based on 
+     * which button has been pressed. 
+     * 
+     * Use State Machine Framework.
+     * 
+     * Current scenes created:
+     * Main Menu
+     * Gameplay
+     * Tutorial
+     * Settings
+     * 
+     * Feel free to remove scenes if not necessary and adjust canvas UI elements.
+     * Reach out if needing to discuss something or have any questions!
+    */
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
