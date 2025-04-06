71f5fdca1ef9039d75b6317ef474b483eb6778de
Ricardo M. <rmoctezumaf@gmail.com>
2025-04-05 19:27:28 -0500

    Revert "Consolidated character subclasses into CharacterCard.cs"
    This reverts commit b6580fb90113fc270acadad29db24e062dd40a83.

---
+++ ReachPathwaysGameProject/Assets/Scripts/Communicator.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.TextCore.Text;
+
+public class Communicator : CharacterCard
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Character = CharacterType.Communicator;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
