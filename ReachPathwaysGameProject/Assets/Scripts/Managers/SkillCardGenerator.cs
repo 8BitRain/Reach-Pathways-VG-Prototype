7934c5b2cd98a02edeb8313d98ed39c49ce19b16
Evy Avila <71989326+EvyAvila@users.noreply.github.com>
2025-04-18 17:24:23 -0500

    Completing Week 10 sprint - Update Skill Cards and Created Points Calculator (#60)
    * Adjusted Skill card prefab
    
    * Adjusted Skill Card script to include abstract inheritance, added specialty cards into the deck and moved scripts to Cards folder
    
    * Updated skill cards prefabs sprites
    
    Had to fill in missing sprites that were removed during the switch of versions.
    
    * Updated Skill Card Generator script
    
    * Added script to calculate points based on list of skill cards point value
    
    * Updated Skill cards objects to be assigned from JSON file and Added UI display for Points

--- ReachPathwaysGameProject/Assets/Scripts/Managers/SkillCardGenerator.cs
+++ ReachPathwaysGameProject/Assets/Scripts/Managers/SkillCardGenerator.cs
@@ -2,60 +2,78 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-
 public class SkillCardGenerator : MonoBehaviour
 {
+    public JSONFileParse file;
+
     List<GameObject> cards = new List<GameObject>();
 
     public GameObject cardPrefab;
+    public GameObject cardSpecial;
 
     public int totalCount;
-    private int divideNum;
+
+    [SerializeField]
+    List<Transform> cardLocation = new List<Transform>();
 
     // Start is called before the first frame update
     void Start()
-    {
-        divideNum = totalCount / 5;
-        GenerateCards();
-        AssignCardColor();
-        
+    {        
+        GenerateCards(totalCount, cardPrefab);
+        GenerateCards(10, cardSpecial);
+
+        AssignCardColor(false, 0, cards.Count - 10, totalCount / 5);
+        AssignCardColor(true, cards.Count - 10, cards.Count, 2);
+
+       
+       for(int i = 0; i < cards.Count; i++)
+       {
+            file.GetFile(cards[i].GetComponent<SkillCardBase>(), i);
+       }
+
     }
 
-    private void GenerateCards()
+    
+
+    private void GenerateCards(int totalCount, GameObject prefab)
     {
         for(int i = 0; i < totalCount; i++)
         {
-            GameObject temp = Instantiate(cardPrefab);
+            GameObject temp = Instantiate(prefab);
             cards.Add(temp);
         }
 
-        cardPrefab.SetActive(false);
+        prefab.SetActive(false);
     }
 
-    private void AssignCardColor()
+    private void AssignCardColor(bool hasSpecialAbility, int startPos, int endPost, int divideNum) //, int x)
     {
         int c = 0;
+        int p = -1;
 
-        //this temp is for display only, can adjust to remove this and line 63 later
-        int tempx = 0;
-        int tempy = -8;
-
-        for (int i = 0; i < cards.Count; i++)
+        for (int i = startPos; i < endPost; i++)
         {
-           
-            if(i % divideNum == 0)
+
+            if (i % divideNum == 0)
             {
                 c++;
-                tempy += 8;
-                tempx = 0;
+                p++;
             }
-            cards[i].GetComponent<SkillCard>().SetCardColor(c);
+            
+            SetCardValues(hasSpecialAbility, c, i);
 
-            cards[i].GetComponent<SkillCard>().SetName(i);
-
-            cards[i].transform.position = new Vector3(tempx, tempy, 0);
-            tempx += 10;
+            cards[i].transform.position = cardLocation[p].position;
         }
     }
+
+    private void SetCardValues(bool hasSpecialAbility, int c, int i)
+    {
+        cards[i].GetComponent<SkillCardBase>().SetCardColor(c);
+
+        cards[i].GetComponent<SkillCardBase>().SetName(i);
+
+        cards[i].GetComponent<SkillCardBase>().SetSpecialty(hasSpecialAbility, i);
+    }
 }
+
+
