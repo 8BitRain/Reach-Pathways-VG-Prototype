0b37f4c730436e48f59130521e9f443b90b129f4
Evy Avila <384036@colum.edu>
2025-05-02 02:46:46 -0500

    Added ability to navigate through the inventory with buttons

--- ReachPathwaysGameProject/Assets/Scripts/Managers/Inventory.cs
+++ ReachPathwaysGameProject/Assets/Scripts/Managers/Inventory.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Flexalon;
+using UnityEngine.UI;
 
-public class Inventory : MonoBehaviour
+public class Inventory : MonoBehaviour //For UI
 {
     [SerializeField]
     private GameObject Canvas, flexParent;
@@ -16,15 +17,22 @@ public class Inventory : MonoBehaviour
     [SerializeField]
     private FlexalonGridLayout gridLayout;
 
-    private int gridColumNum;
+    private int gridColumNum, gridSize, currentPos;
+
+    [SerializeField]
+    private Button bLeft, bRight;
 
     // Start is called before the first frame update
     void Start()
     {
         Canvas.SetActive(false);
         ViewCanvas = false;
-
-        
+        gridSize = (int)gridLayout.Rows * (int)gridLayout.Columns;
+        currentPos = 0;
+        if(CollectedCards.Count <= gridSize)
+        {
+            bRight.interactable = false;
+        }
     }
 
     // Update is called once per frame
@@ -37,16 +45,92 @@ public class Inventory : MonoBehaviour
     {
         ViewCanvas = !ViewCanvas;
         Canvas.SetActive(ViewCanvas);
+        
+        if(!ViewCanvas)
+        {
+            foreach(var v in CollectedCards)
+            {
+                v.gameObject.SetActive(true);
+                currentPos = 0;
+                bLeft.interactable = false;
+                bRight.interactable = true;
+            }
+        }
     }
 
     public void AddToInventory(SkillCardBase card)
     {
         card.transform.SetParent(flexParent.transform, false);
         CollectedCards.Add(card);
-        
+
+        if (CollectedCards.Count > (currentPos + gridSize))
+        {
+            bRight.interactable = true;
+        }
+
+        /*
         if(CollectedCards.Count % gridColumNum == 0)
         {
             gridLayout.Rows++;
-        }
+        }*/
     }
+
+    /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+    public void ShowLeftPageInventory()
+    {
+        if (currentPos == 0)
+        {
+            return;
+        }
+
+        for (int i = currentPos; i >= currentPos - gridSize; i--)
+        {
+            CollectedCards[i].gameObject.SetActive(true);
+
+        }
+        currentPos = GetNewCurrentPosition(currentPos, gridSize, false);
+        UpdateButtons();
+    }
+
+    public void ShowRightPageInventory()
+    {
+        if(currentPos + gridSize >= CollectedCards.Count)
+        {
+            return;
+        }
+
+
+        for (int i = currentPos; i < currentPos + gridSize; i++)
+        {
+            CollectedCards[i].gameObject.SetActive(false);
+
+        }
+        currentPos = GetNewCurrentPosition(currentPos, gridSize, true);
+        UpdateButtons();
+    }
+
+    private void UpdateButtons()
+    {
+        bLeft.interactable = currentPos > 0;
+        bRight.interactable = currentPos + gridSize < CollectedCards.Count;
+    }
+
+    private int GetNewCurrentPosition(int current, int nextPosition, bool isAdding)
+    {
+        var temp = current;
+        switch (isAdding)
+        {
+            case true:
+                temp += nextPosition;
+                break;
+            case false:
+                temp -= nextPosition;
+                break;
+        }
+
+
+        return temp;
+    }
+
+   
 }
