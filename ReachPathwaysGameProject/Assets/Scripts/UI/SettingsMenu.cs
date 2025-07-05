5c44586c320e8c2d89adeef694b75d2c6cd03637
brownseanh <117126750+brownseanh@users.noreply.github.com>
2025-07-05 18:53:17 -0500

    Implemented UI SFX
    Still need to implement mouse over and babble speech

--- ReachPathwaysGameProject/Assets/Scripts/UI/SettingsMenu.cs
+++ ReachPathwaysGameProject/Assets/Scripts/UI/SettingsMenu.cs
@@ -8,5 +8,7 @@ public class SettingsMenu : MonoBehaviour
     public void Back()
     {
         SceneManager.UnloadSceneAsync("Settings");
+
+        AudioManager.Instance.PlaySFX(AudioManager.Instance.menuBack);
     }
 }
