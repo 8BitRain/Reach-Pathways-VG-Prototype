fd1586ea609208db50763df1da0b755c46ec49f8
Ricardo M. <rmoctezumaf@gmail.com>
2025-07-09 18:14:38 -0500

    Added ChangeDialog method, Scenario & InitialDraw dialogs,

--- ReachPathwaysGameProject/Assets/Scripts/UI/Tutorial.cs
+++ ReachPathwaysGameProject/Assets/Scripts/UI/Tutorial.cs
@@ -6,24 +6,57 @@ using UnityEngine;
 
 public class Tutorial : MonoBehaviour
 {
+    [SerializeField]
     public bool tutorialActive = true;
 
     [SerializeField]
     private Dictionary<string, GameObject> dialogs = new();
+    private GameObject activeDialog;
 
     // Start is called before the first frame update
     void Start()
     {
-        // Get a dictionary of all the dialogs present under the parent Tutorial object with their name and the object
-        foreach (RectTransform t in GetComponentsInChildren<RectTransform>()[1..^0])
+        if (tutorialActive)
         {
-            dialogs.Add(t.gameObject.name, t.gameObject);
+            // Get a dictionary of all the dialogs present under the parent Tutorial object with their name and the object
+            foreach (RectTransform t in gameObject.transform)
+            {
+                dialogs.Add(t.gameObject.name, t.gameObject);
+                t.gameObject.SetActive(false);
+            }
+
+            Debug.Log(string.Join(Environment.NewLine, dialogs));
+
+            ChangeDialog("ScenarioDialog");
+
+            StateManager.Instance.OnStateChanged += OnStateChanged;
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnStateChanged(State newState)
     {
-        
+        switch (newState)
+        {
+            case InitialDrawState:
+                ChangeDialog("InitialDrawDialog");
+                Debug.Log("akshdais;uhdka;s");
+                break;
+        }
+    }
+
+    private void ChangeDialog(string dialogName)
+    {
+        if (activeDialog != null)
+        {
+            activeDialog.SetActive(false);
+        }
+
+        activeDialog = dialogs[dialogName];
+        activeDialog.SetActive(true);
+    }
+
+    void OnDestroy()
+    {
+        StateManager.Instance.OnStateChanged -= OnStateChanged;
     }
 }
